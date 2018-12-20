4838381e801a106ba918cff40592036994dee79f
MrSprinkleToes <mr.sprinkletoes@gmail.com>
2018-12-20 10:45:57 -0500

    Create module.py

---
+++ module.py
@@ -0,0 +1,25 @@
+import math
+
+def info():
+  "Displays information about the module easymath."
+  print("easymath is a module created to simplify math in Python. (i mean math in python is already easy so this is probably just making it more complex but oh well!")
+
+def add(firstnumber,secondnumber):
+  "Adds together two integers."
+  return firstnumber + secondnumber
+
+def subtract(firstnumber,secondnumber):
+  "Subtracts two integers."
+  return firstnumber - secondnumber
+
+def multiply(firstnumber,secondnumber):
+  "Multiplies two integers."
+  return firstnumber * secondnumber
+
+def divide(firstnumber,secondnumber):
+  "Divides two integers."
+  return firstnumber / secondnumber
+
+def squareroot(numberToFindSquareRoot):
+  "Finds the square root of an integer."
+  return math.sqrt(numberToFindSquareRoot)
